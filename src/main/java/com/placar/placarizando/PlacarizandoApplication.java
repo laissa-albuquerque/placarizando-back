878e3d13abc2fd0c14fb8fae8fa662f389f19131
laissa-albuquerquer <laissaalbuquerque@gmail.com>
2025-04-15 21:26:44 -0300

    first commit

---
+++ src/main/java/com/placar/placarizando/PlacarizandoApplication.java
@@ -0,0 +1,13 @@
+package com.placar.placarizando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacarizandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacarizandoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/placar/placarizando/PlacarizandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.placar.placarizando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacarizandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
